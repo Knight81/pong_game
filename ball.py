f3a492b6e1688b01eebe4430fc701efce7bc88f0
Knight81 <aitor.alonsof@gmail.com>
2021-05-10 20:10:06 +0200

    Implement ball respawning

--- ball.py
+++ ball.py
@@ -24,7 +24,7 @@ class Ball(Turtle):
         self.x_move *= -1
         self.move_speed *= 0.9
 
-    def reset_position(self):
+    def respawn(self):
         self.goto(0, 0)
         self.move_speed = 0.1
         self.bounce_x()
--- main.py
+++ main.py
@@ -30,8 +30,18 @@ while game_is_on:
     screen.update()
     ball.move()
 
-    # Detect collision with top wall
+    # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -270:
         ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(paddle_right) < 50 and ball.xcor() > 320 or ball.distance(paddle_left) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 400:
+        ball.respawn()
+
+    if ball.xcor() < -400:
+        ball.respawn()
 
 screen.exitonclick()
